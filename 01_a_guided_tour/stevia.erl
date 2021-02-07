cc287fb00018438a2c45c47c3f2093e8ff6dc3cb
Michał Łępicki <michal.lepicki@gmail.com>
2021-02-07 14:48:51 +0100

    finish chapter 01

--- 01_a_guided_tour/stevia.erl
+++ 01_a_guided_tour/stevia.erl
@@ -1,5 +1,5 @@
 -module(stevia).
--export([cheat/1, int_div/2, modulo/2, float_of_int/1, float_to_int/1, float_add/2, float_sub/2, float_div/2, sqrt/1, pow/2, string_split/3, string_lowercase/1]).
+-export([cheat/1, int_div/2, modulo/2, float_of_int/1, float_to_int/1, float_add/2, float_sub/2, float_mul/2, float_div/2, sqrt/1, pow/2, string_split/3, string_lowercase/1, io_get_line/1, float_of_string/1]).
 
 cheat(X) -> X.
 
@@ -15,6 +15,8 @@ float_add(X, Y) -> X + Y.
 
 float_sub(X, Y) -> X - Y.
 
+float_mul(X, Y) -> X * Y.
+
 float_div(X, Y) -> X / Y.
 
 sqrt(X) -> math:sqrt(X).
@@ -24,3 +26,12 @@ pow(X, Y) -> math:pow(X, Y).
 string_split(S, Pattern, Where) -> string:split(S, Pattern, Where).
 
 string_lowercase(S) -> string:lowercase(S).
+
+io_get_line(Prompt) ->
+  case io:get_line(Prompt) of
+    {error, _} -> none;
+    eof        -> none;
+    Data       -> {some, list_to_binary(string:trim(Data))}
+  end.
+
+float_of_string(String) -> binary_to_float(String).
