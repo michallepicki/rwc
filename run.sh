5443c814f4cbba680aa1635c81abe31d4aebb8e8
Michał Łępicki <michal.lepicki@gmail.com>
2021-04-03 17:49:12 +0200

    run when full directory is passed as well

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 dir_prefix="${1:?Missing chapter number argument (e.g. 01)}"
-cd "$dir_prefix"*
+[ -d $dir_prefix ] && cd $dir_prefix || cd "$dir_prefix"*
 caramel_location=$(dirname $(which caramel))
 rm build/*
 cp "$caramel_location"/../lib/caramel/stdlib/caramel_runtime.erl build/caramel_runtime.erl
